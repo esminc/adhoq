807dd7bb40d483a6c254cf00c041643ed20d5361
moro <moronatural@gmail.com>
2014-10-06 18:07:36 +0900

    Fixup test location

--- spec/models/adhoq/execution_spec.rb
+++ spec/models/adhoq/execution_spec.rb
@@ -1,7 +1,4 @@
-require 'rails_helper'
-
 module Adhoq
   RSpec.describe Execution, :type => :model do
-    pending "add some examples to (or delete) #{__FILE__}"
   end
 end
--- spec/models/adhoq/query_spec.rb
+++ spec/models/adhoq/query_spec.rb
@@ -1,13 +1,4 @@
 module Adhoq
   RSpec.describe Query, type: :model do
-    let(:query) { create(:adhoq_query) }
-
-    specify { expect(query).to be }
-
-    describe '#execute' do
-      let(:query) { create(:adhoq_query, query: 'SELECT 42 AS answer') }
-
-      xit { expect(query.execute).to eq Adhoq::Result.new(%w[answer], [[42]]) }
-    end
   end
 end
