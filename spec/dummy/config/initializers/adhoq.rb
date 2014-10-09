1192a02acfd4d31a769099f1a62b1f5e7851fbb4
moro <moronatural@gmail.com>
2014-10-09 18:30:44 +0900

    Add S3 setting example

--- spec/dummy/config/initializers/adhoq.rb
+++ spec/dummy/config/initializers/adhoq.rb
@@ -2,4 +2,16 @@ Adhoq.configure do |c|
   c.authorization = :adhoq_authorized?
   c.current_user  = :current_user
   c.storage       = [:local_file, Rails.root + "./tmp/adhoq/#{Rails.env}"]
+=begin
+  # S3 setting example
+  c.storage       = [
+    :s3,
+    'moro-test',
+    {
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:               'us-east-1',
+    }
+  ]
+=end
 end
