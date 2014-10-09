187c1d6bbd51fb2bcda5c18449689fe56461de93
moro <moronatural@gmail.com>
2014-10-09 23:39:20 +0900

    Golden path e2e testing

--- spec/features/execute_adhoc_query_spec.rb
+++ spec/features/execute_adhoc_query_spec.rb
@@ -1,6 +1,6 @@
 feature 'Golden-path: execute adhoc query' do
-  def table_contant(table_el)
-    table_el.all('tr').map {|row| row.all('th, td').map(&:text) }
+  def table_contant(table)
+    first(table).all('tr').map {|row| row.all('th, td').map(&:text) }
   end
 
   scenario 'Visit root, input query and generate report then we get a report' do
@@ -11,13 +11,30 @@ feature 'Golden-path: execute adhoc query' do
     fill_in 'Query',       with: 'SELECT 42 AS "answer number", "Hello adhoq" AS message'
 
     first('a.js-preview-button').click
-    within('.js-preview-result') do
+    within '.js-preview-result' do
       expect(page).to have_content('Hello')
 
-      expect(table_contant(first('table'))).to eq [
+      expect(table_contant('table')).to eq [
         ['answer number', 'message'],
         ['42',            'Hello adhoq']
       ]
     end
+
+    click_on 'Save'
+
+    within '.new-execution' do
+      select   'xlsx', from: 'Report format'
+      click_on 'Create report'
+    end
+
+    within '.past-executions' do
+      expect(table_contant('table.executions').size).to eq 2
+    end
+
+    # NOTE xlsx parser casts "42" to 42.0
+    expect(Adhoq::Report.order('id DESC').first.data).to have_values_in_xlsx_sheet([
+      ['answer number', 'message'],
+      [42.0,            'Hello adhoq']
+    ])
   end
 end
--- spec/support/have_values_in_xlsx_sheet_matcher.rb
+++ spec/support/have_values_in_xlsx_sheet_matcher.rb
@@ -1,9 +1,15 @@
 require 'rspec/matchers'
 require 'simple_xlsx_reader'
 
-RSpec::Matchers.define :have_values_in_xlsx_sheet do |values|
+RSpec::Matchers.define :have_values_in_xlsx_sheet do |expect_values|
   match do |data|
-    expect(extract_values(data)).to eq values
+    @actual_values = extract_values(data)
+    expect(@actual_values).to eq expect_values
+  end
+
+
+  failure_message do
+    RSpec::Expectations.differ.diff_as_object(@actual_values, expect_values)
   end
 
   private
