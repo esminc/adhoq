8f7a7b74a5418adbc36f9a7d8a43a40f8d48d16b
moro <moronatural@gmail.com>
2014-10-16 10:38:10 +0900

    Refactoring: add report generation facade

--- app/models/adhoq/report.rb
+++ app/models/adhoq/report.rb
@@ -5,7 +5,6 @@ module Adhoq
     belongs_to :execution
 
     delegate :name,      to: 'execution'
-    delegate :mime_type, to: :reporter
 
     def generate!(storage = Adhoq.current_storage)
       self.identifier   = generate_and_persist_report!(storage)
@@ -23,17 +22,15 @@ module Adhoq
       storage.get(identifier)
     end
 
-    private
-
-    def reporter
-      {'xlsx' => Adhoq::Reporter::Xlsx}[execution.report_format]
+    def mime_type
+      Adhoq::Reporter.lookup(execution.report_format).mime_type
     end
+
+    private
 
     def generate_and_persist_report!(storage)
       storage.store(".#{execution.report_format}") do |file, *|
-        executor = Executor.new(execution.raw_sql)
-
-        reporter.new(executor.execute).build_report.each(BUFSIZE) do |chunk|
+        Adhoq::Reporter.generate(execution).each(BUFSIZE) do |chunk|
           file.write chunk
         end
       end
