93a4e79a8b4d45a4a681440ec6e1a417148de688
takkanm <takkanm@gmail.com>
2016-07-20 20:00:26 +0900

    fix duplicate config for Rails5.1

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -13,12 +13,19 @@ Dummy::Application.configure do
   config.eager_load = true
 
   # Configure static asset server for tests with Cache-Control for performance.
-  if config.respond_to?(:serve_static_files)
-    config.serve_static_files = true
+  #
+  if config.respond_to?(:public_file_server)
+    # after Rails5 confguration
+    config.public_file_server.enabled = true
+    config.public_file_server.headers = {'Cache-Control' => 'public, max-age=3600'}
   else
-    config.serve_static_assets = true
+    if config.respond_to?(:serve_static_files)
+      config.serve_static_files = true
+    else
+      config.serve_static_assets = true
+    end
+    config.static_cache_control = 'public, max-age=3600'
   end
-  config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
