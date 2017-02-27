447aade95ccb5331fa89403aaa5936da528075cf
takkanm <takkanm@gmail.com>
2017-02-27 21:16:57 +0900

    fix Migrate Class in dummy app

--- spec/dummy/db/migrate/20150908073801_create_secret_tables.rb
+++ spec/dummy/db/migrate/20150908073801_create_secret_tables.rb
@@ -1,4 +1,7 @@
-class CreateSecretTables < ActiveRecord::Migration
+major, minor, _ = Rails.version.split('.').map(&:to_i)
+migration_class = (major > 5 || major == 5 && minor >= 1) ? ActiveRecord::Migration[4.2] : ActiveRecord::Migration
+
+class CreateSecretTables < migration_class
   def change
     create_table :secret_tables do |t|
 
