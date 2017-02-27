fa5e14d6f51fe94b0a6c0045e06f4e7a0e0be8e9
takkanm <takkanm@gmail.com>
2017-02-27 20:30:43 +0900

    select Migration Class by Rails version.
    Directly inheriting from ActiveRecord::Migration is not supported since Rails5.1.

--- db/migrate/20141006014750_create_adhoq_executions.rb
+++ db/migrate/20141006014750_create_adhoq_executions.rb
@@ -1,4 +1,7 @@
-class CreateAdhoqExecutions < ActiveRecord::Migration
+major, minor, _ = Rails.version.split('.').map(&:to_i)
+migration_class = (major > 5 || major == 5 && minor >= 1) ? ActiveRecord::Migration[4.2] : ActiveRecord::Migration
+
+class CreateAdhoqExecutions < migration_class
   def change
     create_table :adhoq_executions do |t|
       t.belongs_to :query,          null: false
